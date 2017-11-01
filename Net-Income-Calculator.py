9fb271d8ee1ae4de386ef316dfce4468f4a5c1e0
thecody117 <codybenway117@gmail.com>
2017-10-31 21:51:14 -0400

    Bug fixes for the Net-Income-Calculator.

--- Net-Income-Calculator.py
+++ Net-Income-Calculator.py
@@ -7,10 +7,31 @@ ctypes.windll.kernel32.SetConsoleTitleW("New York Net Pay Calculator")
 clear = lambda: os.system('cls')
 clear()
 print("Welcome to the New York Net Pay Calculator.")
-Hours = input("How many hours did you work?: ")
-HourlyPay = input("What is your hourly pay?: ")
+while True:
+    try:
+        Hours = float(input("How many hours did you work?: "))
+        if Hours < 0:
+            raise ValueError()
+        break
+    except ValueError:
+        print("Please enter something valid, such as a positive number.")
+while True:
+    try:
+        HourlyPay = float(input("What is your hourly pay?: "))
+        if HourlyPay < 0:
+            raise ValueError()
+        break
+    except ValueError:
+        print("Please enter something valid, such as a positive number.")
 OvertimePay = float(HourlyPay) * 1.5
-Tax = input("What is the tax rate that is going to be used?: ")
+while True:
+    try:
+        Tax = float(input("What is the tax rate that is going to be used?: "))
+        if Tax < 0:
+            raise ValueError()
+        break
+    except ValueError:
+        print("Please enter something valid, such as a positive number.")
 if float(Hours) > 40:
     OvertimeHours = float(Hours) - 40
     Hours = 40
@@ -27,8 +48,8 @@ elif float(Hours) < 40:
     GrossPay = float(Hours) * float(HourlyPay)
     print('Calculating Gross Pay...')
     print('Your Gross Pay is ' + "$" + str(GrossPay))
-    print("Calculating Net Pay")
+    print("Calculating Net Pay...")
     GrossPayTax = float(GrossPay) * float(Tax)
     NetPay = float(GrossPay) - float(GrossPayTax)
     print("Your Net Pay is " + "$" +str(NetPay))
-input("Awaiting input...")
+input("Calculations complete, please write these down or just remember them. \n Awaiting input...")
